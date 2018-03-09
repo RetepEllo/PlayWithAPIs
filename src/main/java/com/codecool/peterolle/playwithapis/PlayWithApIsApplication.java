583341c3075e9e35b9cd66a927f2c2aa250a131e
ollepeter <mr.peterolle@gmail.com>
2018-03-09 15:15:41 +0100

    init sprinboot project

---
+++ src/main/java/com/codecool/peterolle/playwithapis/PlayWithApIsApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.peterolle.playwithapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayWithApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayWithApIsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/codecool/peterolle/playwithapis/PlayWithApIsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codecool.peterolle.playwithapis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlayWithApIsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
